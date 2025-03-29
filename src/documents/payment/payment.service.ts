ad164edbee69c1d6724319162446aaba3bf5861c
dimmex <dima.sergienko@gmail.com>
2025-03-29 17:00:10 +0200

    feat: add getPayments()

---
+++ src/documents/payment/payment.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { Payment } from './entities';
+
+@Injectable()
+export class PaymentService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentsRepository: Repository<Payment>,
+  ) {}
+
+  async getPayments() {
+    const payments = await this.paymentsRepository
+      .createQueryBuilder('payment')
+      .leftJoin('payment.seller', 'seller')
+      .leftJoin('payment.buyer', 'buyer')
+      .leftJoin('payment.paymentLines', 'paymentLine')
+      .leftJoin('paymentLine.invoice', 'invoice')
+      .select([
+        'payment.id',
+        'payment.status',
+        'payment.documentSum',
+        'payment.expected_date',
+        'seller.name',
+        'buyer.name',
+        'paymentLine.id',
+        'invoice.invoiceNumber',
+      ])
+      .orderBy('payment.id', 'DESC')
+      .getMany();
+
+    return payments;
+  }
+}
