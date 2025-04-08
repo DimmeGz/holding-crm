dee58a375d7aaf0022b9b334a0b384652473e4b6
dimmex <dima.sergienko@gmail.com>
2025-04-08 19:11:35 +0300

    feat: get order confirmations by orderId

--- src/documents/orders-confirmation/orders-confirmation.module.ts
+++ src/documents/orders-confirmation/orders-confirmation.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { OrderConfirmation } from './entities';
+import { OrdersConfirmationService } from './orders-confirmation.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderConfirmation])],
+  providers: [OrdersConfirmationService],
+  exports: [OrdersConfirmationService],
 })
 export class OrdersConfirmationModule {}
