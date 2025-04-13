2dafd91a399f682454120aecb421b6c0a220b82d
dimmex <dima.sergienko@gmail.com>
2025-04-13 11:15:55 +0300

    feat: create/delete TransitLines while shipment status change

--- src/documents/transit/transit.module.ts
+++ src/documents/transit/transit.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { LibsModule } from '../../libs';
+
 import { TransitLine } from './entities';
+import { TransitService } from './transit.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TransitLine])],
+  imports: [TypeOrmModule.forFeature([TransitLine]), LibsModule],
+  providers: [TransitService],
+  exports: [TransitService],
 })
 export class TransitModule {}
