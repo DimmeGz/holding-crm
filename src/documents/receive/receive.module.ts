4f7a959eed3c77de823c5e84752b602b462be534
dimmex <dima.sergienko@gmail.com>
2025-04-13 11:40:44 +0300

    feat: update createReceive(). Add receive relation to TransitLine

--- src/documents/receive/receive.module.ts
+++ src/documents/receive/receive.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { GoodsModule } from '../../goods';
+import { TransitModule } from '../transit';
+import { WarehouseModule } from '../../warehouse';
+
 import { ReceiveService } from './receive.service';
 import { ReceiveController } from './receive.controller';
 
 import { Receive } from './entities';
-import { GoodsModule } from '../../goods';
-import { WarehouseModule } from '../../warehouse';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Receive]), GoodsModule, WarehouseModule],
+  imports: [
+    TypeOrmModule.forFeature([Receive]),
+    GoodsModule,
+    TransitModule,
+    WarehouseModule,
+  ],
   providers: [ReceiveService],
   controllers: [ReceiveController],
   exports: [ReceiveService],
--- src/documents/transit/dto/index.ts
+++ src/documents/transit/dto/index.ts
@@ -1,1 +1,2 @@
+export * from './add-receive-to-transit-line.dto';
 export * from './create-transit-line.dto';
