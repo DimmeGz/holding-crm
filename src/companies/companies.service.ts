4c9c52ff6c22959a621afdefe7597f40053c544c
dimmex <dima.sergienko@gmail.com>
2025-04-13 14:04:09 +0300

    feat: make financial changes in changePaymentStatus()

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,6 +4,7 @@ import { Account, Company } from './entities';
 import { Repository } from 'typeorm';
 
 import { CompanyType } from './enums';
+import { MakePaymentDTO } from './dto';
 
 @Injectable()
 export class CompaniesService {
@@ -57,5 +58,52 @@ export class CompaniesService {
       companyId,
       currencyId,
     });
+  }
+
+  async makePayment(makePaymentDTO: MakePaymentDTO) {
+    const [sellerAccount, buyerAccount] =
+      await this.getSellerBuyerAccounts(makePaymentDTO);
+
+    if (makePaymentDTO.status) {
+      sellerAccount.wait -= makePaymentDTO.amount;
+      sellerAccount.balance += makePaymentDTO.amount;
+
+      buyerAccount.debt -= makePaymentDTO.amount;
+      buyerAccount.balance -= makePaymentDTO.amount;
+    } else {
+      sellerAccount.wait += makePaymentDTO.amount;
+      sellerAccount.balance -= makePaymentDTO.amount;
+
+      buyerAccount.debt += makePaymentDTO.amount;
+      buyerAccount.balance += makePaymentDTO.amount;
+    }
+
+    await this.accountsRepository.save([sellerAccount, buyerAccount]);
+  }
+
+  private async getSellerBuyerAccounts(
+    makePaymentDTO: MakePaymentDTO,
+  ): Promise<Account[]> {
+    const sellerAccount = await this.accountsRepository
+      .createQueryBuilder('account')
+      .where('account.companyId = :companyId', {
+        companyId: makePaymentDTO.sellerId,
+      })
+      .andWhere('account.currencyId = :currencyId', {
+        currencyId: makePaymentDTO.currencyId,
+      })
+      .getOne();
+
+    const buyerAccount = await this.accountsRepository
+      .createQueryBuilder('account')
+      .where('account.companyId = :companyId', {
+        companyId: makePaymentDTO.buyerId,
+      })
+      .andWhere('account.currencyId = :currencyId', {
+        currencyId: makePaymentDTO.currencyId,
+      })
+      .getOne();
+
+    return [sellerAccount, buyerAccount];
   }
 }
