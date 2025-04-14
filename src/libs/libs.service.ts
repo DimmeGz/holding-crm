f98d64657167baf9b420c1c0a63b3b9f2f85ab80
dimmex <dima.sergienko@gmail.com>
2025-04-14 17:41:48 +0300

    refactor: upgrade LibsService and GoodsService

--- src/libs/libs.service.ts
+++ src/libs/libs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TechnicalProcess } from './entities';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
 
 @Injectable()
 export class LibsService {
@@ -10,54 +10,52 @@ export class LibsService {
     private readonly technicalProcessesRepository: Repository<TechnicalProcess>,
   ) {}
 
+  private createBaseQueryBuilder(): SelectQueryBuilder<TechnicalProcess> {
+    return this.technicalProcessesRepository
+      .createQueryBuilder('technicalProcess')
+      .select('technicalProcess.id');
+  }
+
   async getTechnicalProcessesByInvoiceIds(
     invoiceIds: number[],
   ): Promise<TechnicalProcess[]> {
-    return await this.technicalProcessesRepository
-      .createQueryBuilder('technicalProcess')
+    return await this.createBaseQueryBuilder()
       .leftJoin('technicalProcess.invoices', 'invoice')
       .where('invoice.id IN (:...invoiceIds)', { invoiceIds })
-      .select('technicalProcess.id')
       .getMany();
   }
 
   async getTechnicalProcessesByCommissionInvoiceId(
     commissionInvoiceId: number,
   ): Promise<TechnicalProcess[]> {
-    return await this.technicalProcessesRepository
-      .createQueryBuilder('technicalProcess')
+    return await this.createBaseQueryBuilder()
       .leftJoin('technicalProcess.commissionInvoices', 'commissionInvoice')
       .where('commissionInvoice.id = :commissionInvoiceId', {
         commissionInvoiceId,
       })
-      .select('technicalProcess.id')
       .getMany();
   }
 
   async getTechnicalProcessesByProductIds(
     productIds: number[],
   ): Promise<TechnicalProcess[]> {
-    return await this.technicalProcessesRepository
-      .createQueryBuilder('technicalProcess')
+    return await this.createBaseQueryBuilder()
       .leftJoin('technicalProcess.products', 'product')
       .where('product.id IN (:...productIds)', {
         productIds,
       })
-      .select('technicalProcess.id')
       .getMany();
   }
 
   async getTechnicalProcessesByBatchId(
     batchId: number,
   ): Promise<TechnicalProcess[]> {
-    return await this.technicalProcessesRepository
-      .createQueryBuilder('technicalProcess')
+    return await this.createBaseQueryBuilder()
       .leftJoin('technicalProcess.products', 'product')
       .leftJoin('product.batches', 'batch')
       .where('batch.id = :batchId', {
         batchId,
       })
-      .select('technicalProcess.id')
       .getMany();
   }
 }
