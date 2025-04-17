d7a1f32bdf04c65d0e622e8c5c4264b282bd46d4
dimmex <dima.sergienko@gmail.com>
2025-04-17 13:48:45 +0300

    feat: add warehouseAccounting query filter

--- src/warehouse/warehouse.controller.ts
+++ src/warehouse/warehouse.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 import { WarehouseService } from './warehouse.service';
+import { GetWarehouseQueryDTO } from './dto/query-dto';
 
 @Controller('warehouse')
 export class WarehouseController {
   constructor(private readonly warehouseService: WarehouseService) {}
 
   @Get()
-  async getWarehouseAccountings() {
-    return this.warehouseService.getWarehouseAccountings();
+  async getWarehouseAccountings(@Query() query?: GetWarehouseQueryDTO) {
+    return this.warehouseService.getWarehouseAccountings(query);
   }
 }
